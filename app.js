a84261b40a3f1008bf9f30f8f82325f3ac288ac6
unknown <you@example.com>
2020-03-11 16:42:21 +0600

    running assignment

---
+++ app.js
@@ -0,0 +1,50 @@
+
+const express = require('express');
+
+const fs = require('fs');
+
+const app = express();
+
+const requestTime = new Date().toISOString();
+const customMiddleware = (req, res, next) => {
+	console.log(`${req.method} - ${req.url} - ${requestTime}`);
+	next();
+};
+
+
+// customMiddleware
+app.use(customMiddleware);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// GET
+app.get('/', (req, res) => {
+	res.json('GEt');
+});
+
+// GET ID
+app.get('/user/:id', (req, res) => {
+	res.json(`I am a delete ${req.params.id}`);
+});
+
+// fileWrite
+
+app.post('/write', (req, res) => {
+	const users = {
+		name: req.body.name,
+		address: req.body.address,
+		country: req.body.country,
+		queryParameter: req.query,
+	};
+
+	const data = JSON.stringify(users);
+	fs.writeFile(`text${Date.now()}.json`, data, (err) => {
+		if (err) {
+			res.json(err);
+		} else {
+			res.json('File write successful');
+		}
+	});
+});
+
+app.listen(3000);
