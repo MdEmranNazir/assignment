ee2b8852cbc041063a087ccefcc20963317bd541
Md Emran nazir <mdemrannazir9999@gmail.com>
2020-03-13 23:48:58 +0600

    modifies customMiddleware

--- app.js
+++ app.js
@@ -1,50 +1,19 @@
 
 const express = require('express');
+// Port
+const PORT = process.env.PORT || 5000;
 
-const fs = require('fs');
+const customMiddleware = require('./middleware/middelware');
 
 const app = express();
-
-const requestTime = new Date().toISOString();
-const customMiddleware = (req, res, next) => {
-	console.log(`${req.method} - ${req.url} - ${requestTime}`);
-	next();
-};
-
-
+app.use(express.json());
 // customMiddleware
 app.use(customMiddleware);
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// GET
-app.get('/', (req, res) => {
-	res.json('GEt');
+// post router
+app.post('/post', (req, res) => {
+	res.status(200).json({ message: 'Post Successfully', post: req.body });
+	console.log(req.body);
 });
 
-// GET ID
-app.get('/user/:id', (req, res) => {
-	res.json(`I am a delete ${req.params.id}`);
-});
 
-// fileWrite
-
-app.post('/write', (req, res) => {
-	const users = {
-		name: req.body.name,
-		address: req.body.address,
-		country: req.body.country,
-		queryParameter: req.query,
-	};
-
-	const data = JSON.stringify(users);
-	fs.writeFile(`./write-file/text${Date.now()}.json`, data, (err) => {
-		if (err) {
-			res.json(err);
-		} else {
-			res.json('File write successful');
-		}
-	});
-});
-
-app.listen(3000);
+app.listen(PORT, () => { console.log(`server running on port ${PORT}`); });
