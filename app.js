f2c999ffbf5e221d9ee1faefdd90d4cd65be7571
Md Emran Nazir <mdemrannazir9999@gmail.com>
2020-03-17 00:11:48 +0600

    assignment-03

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-
 const express = require('express');
 const customMiddleware = require('./middleware/middelware');
+const errorHandler = require('./middleware/errorHandler');
+
 // Port
 const PORT = process.env.PORT || 5000;
 
@@ -9,11 +10,23 @@ const indexRoute = require('./routers/index');
 
 const app = express();
 app.use(express.json());
+
+// view engine
+app.set('view engine');
+
+
 // customMiddleware
 app.use(customMiddleware);
 
 // Routes
 app.use('/', indexRoute);
 
+// Error
+app.use('*', (req, res, next) => {
+	res.render('notFound.ejs');
+	next();
+});
+// Error handler
+app.use(errorHandler);
 
 app.listen(PORT, () => { console.log(`server running on port ${PORT}`); });
--- routers/index.js
+++ routers/index.js
@@ -1,10 +1,26 @@
 const router = require('express').Router();
 
-
 router.post('/post', (req, res, next) => {
-  res.status(200).json({ message: 'Post Successfully', post: req.body });
-  req.reqParams = req.params;
-  next();
+	res.status(200).json({ message: 'Post Successfully', post: req.body });
+	req.reqParams = req.params;
+	next();
 });
 
+router.get('/', (req, res, next) => {
+	try {
+		res.render('index.ejs', { user: 'Md Emran Nazir' });
+	} catch (err) {
+		next(err);
+	}
+});
+
+// This route will throw an error
+router.get('/error', (req, res, next) => {
+	try {
+	// eslint-disable-next-line
+	  something_goes_wrong_here___
+	} catch (err) {
+		next(err);
+	}
+});
 module.exports = router;
